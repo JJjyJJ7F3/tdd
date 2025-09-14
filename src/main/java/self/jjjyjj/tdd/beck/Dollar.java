dac9f2c6b5f7b6f607b20b08dc02e2d649b27ba4
MegaLeap <zhang_jian_flow@hotmail.com>
2025-09-14 14:20:17 +0800

    Fix incorrect multiplication logic in `Dollar.times` method

--- src/main/java/self/jjjyjj/tdd/beck/Dollar.java
+++ src/main/java/self/jjjyjj/tdd/beck/Dollar.java
@@ -5,10 +5,10 @@ public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
-        this.amount = 10;
+        this.amount = amount * 2;
     }
 }
