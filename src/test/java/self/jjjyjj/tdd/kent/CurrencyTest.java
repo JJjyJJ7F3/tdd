0f2bceb5732855d92981dbbf4f48b463b48f25cf
MegaLeap <zhang_jian_flow@hotmail.com>
2025-09-14 19:41:19 +0800

    Add equality tests for `Franc` in `CurrencyTest`.

--- src/test/java/self/jjjyjj/tdd/kent/CurrencyTest.java
+++ src/test/java/self/jjjyjj/tdd/kent/CurrencyTest.java
@@ -39,5 +39,9 @@ public class CurrencyTest {
         assertEquals(new Dollar(5), new Dollar(5));
 
         assertNotEquals(new Dollar(5), new Dollar(6));
+
+        assertEquals(new Franc(5), new Franc(5));
+
+        assertNotEquals(new Franc(5), new Franc(6));
     }
 }
