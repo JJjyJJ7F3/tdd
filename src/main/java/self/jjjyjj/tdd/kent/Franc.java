6aba432a1a14fcaac5065e28b4b62623fd45ad90
MegaLeap <zhang_jian_flow@hotmail.com>
2025-09-14 21:37:17 +0800

    Refactor `Dollar` and `Franc` constructors to use `super` for initializing fields and move `Money` field initialization to a new constructor.

--- src/main/java/self/jjjyjj/tdd/kent/Franc.java
+++ src/main/java/self/jjjyjj/tdd/kent/Franc.java
@@ -2,8 +2,7 @@ package self.jjjyjj.tdd.kent;
 
 public class Franc extends Money {
     public Franc(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
+        super(amount, currency);
     }
 
     public Money times(int multiplier) {
--- src/main/java/self/jjjyjj/tdd/kent/Money.java
+++ src/main/java/self/jjjyjj/tdd/kent/Money.java
@@ -4,6 +4,13 @@ public abstract class Money {
     protected int amount;
     protected String currency;
 
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public abstract Money times(int amount);
+
     public static Dollar dollar(int amount) {
         return new Dollar(amount, "USD");
     }
@@ -16,8 +23,6 @@ public abstract class Money {
     public boolean equals(Object obj) {
         return this.amount == ((Money) obj).amount && this.getClass() == obj.getClass();
     }
-
-    public abstract Money times(int amount);
 
     public String currency() {
         return currency;
