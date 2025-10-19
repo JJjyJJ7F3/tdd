b560632a12a8039f036d17e3b56e794dcc58d237
MegaLeap <zhang_jian_flow@hotmail.com>
2025-10-20 00:02:53 +0800

    Simplify `Username` class and tests by removing validation logic and dependencies.

--- src/main/java/self/jjjyjj/tdd/alan/Username.java
+++ src/main/java/self/jjjyjj/tdd/alan/Username.java
@@ -1,35 +1,13 @@
 package self.jjjyjj.tdd.alan;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
-/**
- * The username should be converted to lowercase and should be at least 3 characters long.
- * Note this class is a value object, so it should be immutable.
- */
 public class Username {
+    private final String username;
 
-    private final String name;
-
-    public Username(String name) {
-        if (name.length() < 3) {
-            throw new InvalidNameException("Username must be at least 3 characters long");
-        }
-
-        this.name = name;
+    public Username(String username) {
+        this.username = username;
     }
 
     public String toLowerCase() {
-        return this.name.toLowerCase();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(this, obj);
-    }
-
-    @Override
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
+        return username.toLowerCase();
     }
 }
--- src/test/java/self/jjjyjj/tdd/alan/UsernameTest.java
+++ src/test/java/self/jjjyjj/tdd/alan/UsernameTest.java
@@ -3,35 +3,17 @@ package self.jjjyjj.tdd.alan;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class UsernameTest {
     @Test
     public void convertsToLowercase() {
         // Arrange
-        String expected = "john";
-        Username actual = new Username("John");
+        var username = new Username("Singularity");
 
-        // Act & Assert
-        assertThat(actual.toLowerCase()).isEqualTo(expected);
+        // Act
+        String actual = username.toLowerCase();
+
+        // Assert
+        assertThat(actual).isEqualTo("singularity");
     }
-
-    /**
-     * the username should be at least 3 characters long
-     */
-    @Test
-    public void rejectShortUsername() {
-        assertThatExceptionOfType(InvalidNameException.class)
-            .isThrownBy(() -> new Username("AG"))
-            .withMessage("Username must be at least 3 characters long");
-    }
-
-    @Test
-    public void acceptMinimumLengthUsername() {
-        assertThatNoException()
-            .isThrownBy(() -> new Username("ABC"));
-    }
-
-    
 }
